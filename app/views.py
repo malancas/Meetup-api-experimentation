dc6043dd3a106cdb8489df65797468b1f2850410
malancas <malancaster0@gmail.com>
2016-12-24 16:56:42 -0500

    Added results template. Updated writing in search.html and search and results routes

--- app/views.py
+++ app/views.py
@@ -18,12 +18,22 @@ def index():
     return render_template('index.html',
                            title='Home',
                            user=user, cats=cats.json())
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm(request.form)
     if form.validate_on_submit():
-        return redirect('/index')
+        return results(form)
     return render_template('search.html', 
                            title='Search',
                            form=form)
 
+def results(form):
+    #groups = requests.get("https://api.meetup.com/find/groups?key=50435526d4215731a6973f07d5d50&sign=true")
+    
+    groups = requests.get("https://api.meetup.com/find/groups?key=50435526d4215731a6973f07d5d50&sign=true&photo-host=public&zip=10514&page=20")
+    return render_template('results.html',
+                           title='Meetup results',
+                           groups=groups.json())
+
+    
