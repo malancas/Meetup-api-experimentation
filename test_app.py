3b536621c8af0f265e3d1e444be8947e88feb2f3
malancas <malancaster0@gmail.com>
2016-12-28 12:29:37 -0500

    Removed unused html templates. Updates tests in test_app

--- test_app.py
+++ test_app.py
@@ -2,12 +2,13 @@
 import os
 import unittest
 from app import forms
+from wtforms.validators import InputRequired, Length, ValidationError
 
 def test_isValidInteger():
     assert(forms.isValidInteger(2.0) == 2)
     assert(forms.isValidInter(2.5) != 2)
-    unittest.assertRaises(Validation, isValidInteger(2.5))
+    unittest.assertRaises(Validation, forms.isValidInteger(2.5))
 
 class MyTestCase(unittest.TestCase):
     def test1(self):
-        self.assertRaises(ValidaitonError, isValidInteger(2.5))
+        self.assertRaises(ValidationError, forms.isValidInteger(2.5))
